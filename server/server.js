487b7dffa4ffe1ab0caf652ecd997249416b0d0f
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-11-04 13:21:09 +0200

    cors & morgan

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const { handleError } = require("./utils/handleErrors");
 const app = express();
 const router = require("./router/router");
 const cors = require("./middlewares/cors");
+const logger = require("./logger/loggerService");
 
 app.use(cors);
+app.use(logger);
 app.use(express.json());
 app.use(express.static("./public"));
-
 app.use(router);
 
 app.use((err, req, res, next) => {
