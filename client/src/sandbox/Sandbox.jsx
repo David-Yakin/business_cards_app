dbf37536ae284bf8b8d9c834f0a9c014387dff0c
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-12-27 13:42:25 +0200

    prop-types folders name fixed

--- client/src/sandbox/Sandbox.jsx
+++ client/src/sandbox/Sandbox.jsx
@@ -5,18 +5,17 @@
 // import FatherPropsObject from "./props/props-object/FatherPropsObject";
 // import FatherPropsString from "./props/props-string/FatherPropsString";
 // import FatherPropsTwoKeys from "./props/props-two-keys/FatherPropsTwoKeys";
-
-import Loops from "./Loops";
-import OnClick from "./events/OnClick";
-import RaisingEvents from "./events/RaisingEvents";
-import FatherAnyAndDefaultProps from "./propTypes/any-and-defaultProps/FatherAnyAndDefaultProps";
-import FatherArrayOfAndObjectOf from "./propTypes/arrays-and-objects/FatherArrayOfAndObjectOf";
+// import Loops from "./Loops";
+// import OnClick from "./events/OnClick";
+// import RaisingEvents from "./events/RaisingEvents";
+// import FatherPropTypeError from "./propTypes/propType-error/FatherPropTypeError";
+// import FatherMainTypes from "./propTypes/main-types/FatherMainTypes";
+// import FatherArrayOfAndObjectOf from "./propTypes/arrayOf-and-objectOf/FatherArrayOfAndObjectOf";
+// import FatherOneOfVsOneOfType from "./propTypes/oneOf-vs-oneOfType/FatherOneOfVsOneOfType";
+// import FatherExactVsRequired from "./propTypes/exact-vs-isRequired/FatherExactVsRequired";
+// import FatherShape from "./propTypes/shape/FatherShape";
+// import FatherAnyAndDefaultProps from "./propTypes/any-and-defaultProps/FatherAnyAndDefaultProps";
 import FatherChildrenAndNode from "./propTypes/children-and-node/FatherChildrenAndNode";
-import FatherExactVsRequired from "./propTypes/exact-vs-required/FatherExactVsRequired";
-import FatherMainTypes from "./propTypes/main-types/FatherMainTypes";
-import FatherOneOfVsOneOfType from "./propTypes/oneOf-vs-oneOfType/FatherOneOfVsOneOfType";
-import FatherPropTypeError from "./propTypes/propType-error/FatherPropTypeError";
-import FatherShape from "./propTypes/shape/FatherShape";
 
 const Sandbox = () => {
   return (
